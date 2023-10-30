f02412605fa45d17e42b2c63c18e2123fa996db3
tyler36 <tyler36@users.noreply.github.com>
2023-10-30 18:07:51 +0900

    Allow Drupal Core version to be saved (#13)

--- scripts/background.js
+++ scripts/background.js
@@ -1,28 +1,97 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse)=>{
+/**
+ * Array of supported Drupal Core values.
+ */
+const optionsDrupalCoreVersion = [
+  '9.5.9',
+  '9.5.x',
+  '10.0.9',
+  '10.0.x',
+  '10.1.x',
+  '11.x',
+];
+const defaultDrupalCoreVersion = '10.1.x';
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'fetch-drupalpod-repo') {
     (async function responding() {
       sendResponse({message: await getDrupalPodRepo()});
     })();
-  } else if (request.message === 'set-drupalpod-repo') {
-    setDrupalPodRepo(request.url);
+  }
+  
+  if (request.message === 'set-drupalpod-repo') {
+      setDrupalPodRepo(request.url);
+      sendResponse({message: 'great success'});
+  }
+
+  if (request.message === 'get-drupal-core-version-options') {
+    sendResponse({options: optionsDrupalCoreVersion});
+  }
+
+  if (request.message === 'fetch-drupal-core-version') {
+    (async function responding() {
+      sendResponse({message: await getDrupalCoreVersion()});
+    })();
+  }
+
+  if (request.message === 'set-drupal-core-version') {
+    setDrupalCoreVersion(request.core);
     sendResponse({message: 'great success'});
   }
+
   return true;
 });
 
 async function getDrupalPodRepo() {
+  return getOptionFromStorage('drupalpod_repo');
+}
+
+async function getDrupalCoreVersion() {
+  return getOptionFromStorage('drupal_core_version');
+}
+
+/**
+ * Get the option 'name' from Chrome storage.
+ *
+ * @param string name
+ * @returns
+ */
+async function getOptionFromStorage(name) {
   var p = new Promise((resolve, reject) => {
-    chrome.storage.sync.get(['drupalpod_repo'], (options)=>{
-      resolve(options.drupalpod_repo);
-    })
+    chrome.storage.sync.get([name], (options) => {
+      resolve(options[name]);
+    });
   });
 
   return await p;
 }
 
+/**
+ * Set the default DrupalPod repo.
+ * This generally will not need changing.
+ *
+ * @param string url
+ */
 function setDrupalPodRepo(url) {
-    chrome.storage.sync.set({'drupalpod_repo': url });
+  setOptions({drupalpod_repo: url});
 }
+
+/**
+ * Set the default Drupal Core version.
+ *
+ * @param string core
+ */
+function setDrupalCoreVersion(core) {
+  setOptions({drupal_core_version: core});
+}
+
+/**
+ * Helper function to save settings.
+ * @param object options
+ */
+function setOptions(options) {
+  chrome.storage.sync.set(options);
+};
 
 // set default
 setDrupalPodRepo('https://github.com/shaal/drupalpod');
+setDrupalCoreVersion(defaultDrupalCoreVersion);
---
+++ tests/basic.test.js
@@ -0,0 +1,100 @@
+const puppeteer = require("puppeteer");
+
+const EXTENSION_PATH = "D:/code/platform/chrome/drupalPod";
+const EXTENSION_ID = "pjfjhkcfkhbemnbpkakjhmboacefmjjl";
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      `--disable-extensions-except=${EXTENSION_PATH}`,
+      `--load-extension=${EXTENSION_PATH}`,
+    ],
+  });
+});
+
+afterEach(async () => {
+  await browser.close();
+  browser = undefined;
+});
+
+test('it warns when NOT on Drupal issue page', async () => {
+  const page = await browser.newPage();
+  await page.goto(`chrome-extension://${EXTENSION_ID}/popup.html`);
+
+  await page.waitForXPath('//*[contains(text(), "Open an issue page on Drupal.org to see the available options.")]');
+});
+
+test("it sets drupalPod repo options", async () => {
+  const page = await browser.newPage();
+  await page.goto(`chrome-extension://${EXTENSION_ID}/options.html`);
+
+  let selectorDrupalPodRepo = '#drupalpod-repo';
+  await page.waitForSelector(selectorDrupalPodRepo);
+
+  // ASSERT default value.
+  let placeholder = await page.$eval(selectorDrupalPodRepo, (input) => input.getAttribute('placeholder'));
+  expect(placeholder).toEqual("https://github.com/shaal/drupalpod");
+
+  // Save value
+  let input = await page.$('#drupalpod-repo');
+  await input.click({ clickCount: 3 });
+  await input.type('https://github.com/shaal/update-repo');
+  await page.click('button[type="submit"]');
+
+  // ASSERT value matches
+  await page.goto(`chrome-extension://${EXTENSION_ID}/options.html`);
+  let value = await page.$eval(selectorDrupalPodRepo, (input) => input.value);
+  expect(value).toEqual('https://github.com/shaal/update-repo');
+});
+
+test("it contains drupal core options", async () => {
+  const page = await browser.newPage();
+  await page.goto(`chrome-extension://${EXTENSION_ID}/options.html`);
+
+  let selectorDrupalCore = "#drupal-core";
+  await page.waitForSelector(selectorDrupalCore);
+
+  // ASSERT current options.
+  let selectOptions = await page.evaluate(() => {
+    let selectElement  = document.getElementById("drupal-core");
+    return Array.from(selectElement.options).map(
+      (option) => option.value
+    );
+  });
+  expect(["9.5.9", "9.5.x", "10.0.9", "10.0.x", "10.1.x", "11.x"]).toEqual(
+    expect.arrayContaining(selectOptions)
+  );
+});
+
+
+test("it sets drupal core options", async () => {
+  const page = await browser.newPage();
+  await page.goto(`chrome-extension://${EXTENSION_ID}/options.html`);
+
+  let selectorDrupalCore = "#drupal-core";
+
+  // ASSERT default
+  await page.waitForSelector(selectorDrupalCore + " option");
+  selectedOption = await page.evaluate(() => {
+    let select = document.getElementById("drupal-core");
+    let selected = select.options[select.selectedIndex];
+    return selected.value;
+  });
+  expect(selectedOption).toEqual("10.1.x");
+
+  // Save value
+  let input = await page.$(selectorDrupalCore);
+  await input.select(selectorDrupalCore, "11.x");
+  await page.click('button[type="submit"]');
+
+  // ASSERT value matches
+  await page.goto(`chrome-extension://${EXTENSION_ID}/options.html`);
+  await page.waitForSelector(selectorDrupalCore + " option");
+  selectedOption = await page.evaluate(() => {
+    let select = document.getElementById("drupal-core");
+    let selected = select.options[select.selectedIndex];
+    return selected.value;
+  });
+  expect(selectedOption).toEqual("11.x");
+});
